e8e7242e7ed4520f7b8423ba388cff6428af8618
alireza <alireza-jaz@bd.ir>
2023-10-24 15:35:30 +0330

    Each action class must be a reusable unit of our app's business logic. these actions were too small and I could'nt imagine them being reused. so I refactored to private method to decrease complexity and still keeping single responsibility.

--- app/Actions/TransferCreditAction.php
+++ app/Actions/TransferCreditAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use App\Enums\TransactionStatus;
 use App\Exceptions\InsufficientBalanceException;
 use App\Models\Card;
 use App\Models\Transaction;
@@ -10,18 +11,11 @@ use Illuminate\Support\Facades\DB;
 
 class TransferCreditAction
 {
-    public function __construct(
-        protected StorePendingTransactionAction $storePendingTransactionAction,
-        protected MarkTransactionAsCompletedAction $markTransactionAsCompletedAction
-    )
-    {
-    }
-
     public function __invoke(Card $senderCard, Card $receiverCard, int $amount): Transaction
     {
         $this->ensureSufficientBalance($senderCard, $amount);
 
-        $transaction = ($this->storePendingTransactionAction)($senderCard, $receiverCard, $amount);
+        $transaction = $this->storePendingTransaction($senderCard, $receiverCard, $amount);
 
         DB::beginTransaction();
 
@@ -46,10 +40,22 @@ class TransferCreditAction
 
     private function ensureSufficientBalance(Card $sender, int $amount): void
     {
-        if ($sender->account->balance < ($amount + config('fee.amount'))) {
-            throw InsufficientBalanceException::withMessages([
-                'sender_card_number' => __('transfer.insufficient_balance')
-            ]);
+        if ($sender->account->balance > ($amount + config('fee.amount'))) {
+            return;
         }
+
+        throw InsufficientBalanceException::withMessages([
+            'sender_card_number' => __('transfer.insufficient_balance')
+        ]);
+    }
+
+    private function storePendingTransaction(Card $sender, Card $receiver, int $amount): Transaction
+    {
+        return Transaction::query()->create([
+            'sender_card_id' => $sender->id,
+            'receiver_card_id' => $receiver->id,
+            'amount' => $amount,
+            'status' => TransactionStatus::PENDING,
+        ]);
     }
 }
